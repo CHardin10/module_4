d30d38b42bfb5ccdee575532c594fdc3d5ca3933
Sam Ballard <samcballard@gmail.com>
2019-09-16 16:42:43 -0400

    latest

--- practice/SecretNumber.java
+++ practice/SecretNumber.java
@@ -8,25 +8,47 @@ public class SecretNumber {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
+		double userGuess, randomNum;
 		
-		double userGuess, compare, randomNum;
+		System.out.println();
+		System.out.print("Guess a number between 0 and 10: ");
 		
-		System.out.print("Guess a number between 1 and 10: ");
-		
-		userGuess = Math.round(sc.nextDouble());
-		randomNum = Math.random();
-		compare = Math.round(randomNum * 10);
-		
-			if (userGuess == compare) {
+		randomNum = Math.round(Math.random() * 10);
+					
+			while ((userGuess = Math.round(sc.nextDouble())) != randomNum) {
+	
+				System.out.println();
+				System.out.print("Sorry, guess again: ");
+	
+					if (userGuess > 10 || userGuess < 0) {
+							
+						System.out.println();
+						System.out.println("Enter a valid number!");
+						SecretNumber.main(args);
+				}
+			}
+			
+			if (userGuess == randomNum) {
 				
+				System.out.println();
 				System.out.println("You chose " + (int)userGuess + ", that's correct!");
-				
-			} else {
-				
-				System.out.println("Sorry, the number was " + (int)compare);
 				
 			}
 			
+		String yes = "yes";
+		String no = "no";
+		
+		System.out.print("Do you want to guess another number? (Yes/No): ");
+		String choose = sc.nextLine().toLowerCase();
+					
+			if (choose.contentEquals(yes)) {
+							
+				SecretNumber.main(args);
+							
+			} else if (choose.contentEquals(no)) {
+							
+				System.exit(0);
+							
+			}
 	}
-
 }
